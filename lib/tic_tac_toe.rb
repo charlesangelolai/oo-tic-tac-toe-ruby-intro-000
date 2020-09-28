0f3336f429f080397f5d9ba5857e109433e61d91
Charles Lai <charlesangeloplai@gmail.com>
2020-09-28 20:53:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    if position_taken?(index) || (index < 0 && index > 10)
+    if position_taken?(index) || (index < 0 && index > 9)
       return false
     end
     return true
